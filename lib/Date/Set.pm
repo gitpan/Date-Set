b202e5136a402e19dc8a79b333cc5fa00cd0115b
Flávio Soibelmann Glock <fglock@gmail.com>
2001-11-30 15:36:38 +0000

    Import of FGLOCK/Date-Set-1.10 from CPAN.
    gitpan-cpan-distribution: Date-Set
    gitpan-cpan-version:      1.10
    gitpan-cpan-path:         FGLOCK/Date-Set-1.10.tar.gz
    gitpan-cpan-author:       FGLOCK
    gitpan-cpan-maturity:     released

--- lib/Date/Set.pm
+++ lib/Date/Set.pm
@@ -17,7 +17,7 @@ use Carp;
 @ISA = qw(Set::Infinite);
 @EXPORT = qw();
 @EXPORT_OK = qw(type);
-$VERSION = (qw'$Revision: 1.05 $')[1];
+$VERSION = (qw'$Revision: 1.10 $')[1];
 
 =head1 NAME
 
@@ -121,11 +121,31 @@ sub dtstart { # start
 	# return $self->intersection($tmp);
 }
 
+=head2 dtend
+
+	dtend( end => time1 )
+
+Returns set intersection (Inf .. time1]
+
+'dtend' puts a limit on when the event finishes. 
+If the event already finish BEFORE dtend, it will not change.
+
+=cut
+
+
+sub dtend { # end
+	my ($self, %parm) = @_;
+	$self->print(title=>'dtend ' . join(':', %parm) );
+	return $self->intersection($past, $parm{end});
+	# my $tmp = __PACKAGE__->new($parm{start}, $future);
+	# return $self->intersection($tmp);
+}
+
 =head2 duration
 
-	duration( unit => months, duration => 10 )
+	duration( unit => 'months', duration => 10 )
 
-All intervals for the quantize function are modified to 'duration'.
+All intervals are modified to 'duration'.
 
 'unit' parameter can be years, months, days, weeks, hours, minutes, or seconds.
 
@@ -142,7 +162,8 @@ sub duration { # unit,duration
 
 =head2 rrule
 
-    rrule ( BYMONTH => [ list ], BYWEEKNO => [ list ],
+    rrule ( period => date-set,  DTSTART => time,
+        BYMONTH => [ list ],     BYWEEKNO => [ list ],
         BYYEARDAY => [ list ],   BYMONTHDAY => [ list ],
         BYDAY => [ list ],       BYHOUR => [ list ],
         BYMINUTE => [ list ],    BYSECOND => [ list ],
@@ -159,20 +180,32 @@ WKST and BYDAY list may contain: SU MO TU WE TH FR SA
 BYxxx items must be array references (must be bracketed): BYMONTH => [ 10 ] or
 BYMONTH => [ 10, 11, 12 ] or BYMONTH => [ qw(10 11 12) ]
 
-(some documentation needed!)
+There are two operating modes: without 'period' it will filter out the rule from the set;
+with 'period' it will filter out the rule from the period, then add the list to the set.
+
+DTSTART value can be given explicitly, otherwise it will be taken from 'period' or from the set.
 
 =cut
 
-sub rrule { # freq, &method(); optional: interval, until, count
-	# TODO: count, interval
+sub rrule { 
 	my $self = shift;
+	my %parm = @_;
+
+	my $rrule;
+	my %has;
+
+	# this is the constructor interface
+	# it creates an object if we are not given one
 	unless (ref($self)) {
 		# print " new: $self ";
-		unshift @_, $self;
-		$self = $forever;
+		# unshift @_, $self;
+		$self = (exists $parm{period}) ? $never : $forever;
 	}
 	my $class = ref($self);
 
+	# this is the backtracking interface. 
+	# It allows the program to defer processing if it does not have enough
+	# information to proceed.
 	if (($self->{too_complex}) or ($self->min == -&inf) or ($self->max == &inf)) {
 		my $b = $class->new();
 		$self->trace(title=>"rrule:backtrack"); 
@@ -180,29 +213,125 @@ sub rrule { # freq, &method(); optional: interval, until, count
 		$b->{too_complex} = 1;
 		$b->{parent} = $self;
 		$b->{method} = 'rrule';
-		$b->{param}  = \@_;
+		$b->{param}  = [ %parm ];
 		return $b;
 	}
 
-	# print "   ", join(" ; ", @_ ), "  ";
-	my %parm = @_;
-	my $rrule;
-	my $when = $self;
+	# Try to find out what 'period' the rrule is talking about
+	$has{period} = 1;
+	unless (exists $parm{period}) {
+		$parm{period} = $self;
+		$has{period} = 0;
+	}
+	my $when = $parm{period};
 
-	$parm{FREQ} = $parm{FREQ} . '';
-	$parm{INTERVAL} = $parm{INTERVAL} . '';
-	$parm{COUNT} = $parm{COUNT} . '';
-	$parm{UNTIL} = $parm{UNTIL} . '';
-	$parm{WKST} = $parm{WKST} . '';
+	# DTSTART gives the default values for month, day, h, m, s
+	$parm{DTSTART} = $parm{period}->min unless exists $parm{DTSTART};
+	# print " [DTSTART = $parm{DTSTART} ]\n";
+	# my ($has_month, $has_day, $has_hour, $has_minute, $has_second) = (0,0,0,0,0);	
+
+	# define everything we use
+        my @list = qw/FREQ INTERVAL COUNT UNTIL WKST/;
+        for(@list) {
+            next if defined $parm{$_};
+            $parm{$_} = '';
+        }
+
+	# $parm{FREQ} = $parm{FREQ} . '';
+	# $parm{INTERVAL} = $parm{INTERVAL} . '';
+	# $parm{COUNT} = $parm{COUNT} . '';
+	# $parm{COUNT} = 999_999 unless $parm{COUNT};
+	# $parm{UNTIL} = $parm{UNTIL} . '';
+	# $parm{WKST} = $parm{WKST} . '';
+
 	$parm{WKST} = "MO" unless $parm{WKST};
+
+	# print " PARAMETERS: ", join(":", %parm), "\n";
 
 	$when->print(title=>'WHEN');
 
+	# apply DTSTART, just in case
+	$when = $when->intersection( $parm{DTSTART}, $future  );
+
+	# UNTIL and COUNT MUST NOT occur in the same 'recur'  (why?)
 	if ($parm{UNTIL} ne '') {
-		my $until = $when;
-		$when = $until->intersection($past, $parm{UNTIL});
+		# UNTIL
 		$when->print(title=>'UNTIL');
+		$when = $when->intersection( $past, $parm{UNTIL} );
 	}
+
+	if ($parm{FREQ} ne '') {
+		# FREQ, INTERVAL, COUNT
+
+		# $DEBUG = 1;
+
+		$when->print(title=>'FREQ');
+		$parm{COUNT} = 999999 unless $parm{COUNT};
+		$parm{INTERVAL} = 1 unless $parm{INTERVAL};
+
+		if ($self->max == &inf) {
+			# that's bad to quantize -- try to fix it
+		}
+
+		# -- FREQ works here:
+		my $freq = $when->quantize(unit=>$freq{$parm{FREQ}}, strict=>0)
+							->print(title=>'FREQ');
+
+		# -- WKST works here:   --> only if FREQ=WEEKLY; see also: BYWEEKNO
+		if ($parm{FREQ} eq 'WEEKLY') {
+			my $wkst = $weekday{$parm{WKST}};
+			# print " [ wkst: $parm{WKST} = $wkst ] \n";
+			$freq = $freq->offset( unit=>'days', value=>[$wkst,$wkst] );
+			# print " [ wkst: $freq ] \n";
+		}
+
+		$rrule = $when->intersection(
+							$freq
+
+							# -- INTERVAL works here:
+							->select( freq=>$parm{INTERVAL}, count=>999999, strict=>0 ) 
+							->print(title=>'FREQ('.$parm{FREQ}.')+INTERVAL('.$parm{INTERVAL}.')')
+
+							->iterate( 	sub { $_[0]
+											->print(title=>'PART-'.$parm{FREQ}) 
+											->rrule_by ( %parm )
+											->print(title=>'PART-done:') 
+											} 
+							)
+							# remove anything out of range before counting!
+							->intersection($parm{period})
+
+							# -- COUNT works here:
+							->select( freq=>1, count=>$parm{COUNT}, strict=>0 ) 
+							->print(title=>'COUNT('.$parm{COUNT}.')')
+
+							->duration( unit => 'seconds', duration => 0 )
+						);
+	}
+	else {
+		# is this in the RFC?
+		$when->print(title=>'no FREQ or UNTIL');
+		$rrule = $when->intersection(
+							$when->rrule_by(%parm)
+							->duration( unit => 'seconds', duration => 0 )
+						);
+	}
+
+
+
+	if ($has{period}) {
+		return $self->union ( $rrule );
+	}
+	return $rrule;
+}
+
+# INTERNAL sub to calculate BYxxx
+#   input: $when, %parm
+#   output: $when (filtered)
+sub rrule_by {
+
+	my ($when, %parm) = @_;
+	my %has = ();
 
 	# BYMONTH, BYWEEKNO, BYYEARDAY, BYMONTHDAY, BYDAY, BYHOUR,
 	# BYMINUTE, BYSECOND and BYSETPOS; then COUNT and UNTIL are evaluated
@@ -216,6 +345,8 @@ sub rrule { # freq, &method(); optional: interval, until, count
 			->print (title=>'months2 ' . join(',' , @by) )
 		)->no_cleanup; 
 		$when->print(title=>'BYMONTH');
+
+		$has{month} = 1;
 	}
 
 	if (exists $parm{BYWEEKNO}) {
@@ -229,6 +360,9 @@ sub rrule { # freq, &method(); optional: interval, until, count
 			->print (title=>'weeks2 ' . join(',' , @by) )
 		)->no_cleanup; 
 		$when->print(title=>'BYWEEKNO');
+
+		$has{month} = 1;
+		$has{day} = 1;   # maybe
 	}
 
 	if (exists $parm{BYYEARDAY}) {
@@ -239,6 +373,9 @@ sub rrule { # freq, &method(); optional: interval, until, count
 			->offset(mode=>'circle', unit=>'days', value=>[@by], strict=>0 )
 		)->no_cleanup; 
 		$when->print(title=>'BYYEARDAY');
+
+		$has{month} = 1;
+		$has{day} = 1;
 	}
 
 	if (exists $parm{BYMONTHDAY}) {
@@ -251,6 +388,9 @@ sub rrule { # freq, &method(); optional: interval, until, count
 			# ->print (title=>'days')
 		)->no_cleanup; 
 		$when->print(title=>'BYMONTHDAY');
+
+		$has{month} = 1;
+		$has{day} = 1;
 	}
 
 	if (exists $parm{BYDAY}) {
@@ -285,7 +425,7 @@ sub rrule { # freq, &method(); optional: interval, until, count
 
 			# look at FREQ and create $base 
 			my $base;
-			if ($parm{FREQ} eq 'YEARLY') {
+			if (($parm{FREQ} eq 'YEARLY') and not (exists $parm{BYMONTH})) {
 				$base = $BYDAY->quantize(unit => 'years', strict=>0);
 			}
 			else {
@@ -300,9 +440,6 @@ sub rrule { # freq, &method(); optional: interval, until, count
 				# parse parameters
 				my ($index, $day) = /([\-\+]\d+)(\w+)/;
 				$day = $weekday{$day};
-
-				# TODO: get indexed days to $indexed set
-				# quantize weeks -> offset -> intersection -> select
 
 				# print " [Indexed BYDAY: $index $day, base $base ]\n";
 
@@ -334,6 +471,9 @@ sub rrule { # freq, &method(); optional: interval, until, count
 		$when = $non_indexed->union($indexed);
 		$when->print(title=>'BYDAY');
 
+		$has{month} = 1;
+		$has{day} = 1;
+
 	} # end: BYDAY
 
 	if (exists $parm{BYHOUR}) {
@@ -345,6 +485,8 @@ sub rrule { # freq, &method(); optional: interval, until, count
 			# ->print (title=>'hours')
 		)->no_cleanup; 
 		$when->print(title=>'BYHOUR');
+
+		$has{hour} = 1;
 	}
  
 	if (exists $parm{BYMINUTE}) {
@@ -356,6 +498,8 @@ sub rrule { # freq, &method(); optional: interval, until, count
 			# ->print (title=>'minutes')
 		)->no_cleanup; 
 		$when->print(title=>'BYMINUTE');
+
+		$has{minute} = 1;
 	}
 
 	if (exists $parm{BYSECOND}) {
@@ -367,6 +511,8 @@ sub rrule { # freq, &method(); optional: interval, until, count
 			# ->print (title=>'seconds')
 		)->no_cleanup; 
 		$when->print(title=>'BYSECOND');
+
+		$has{second} = 1;
 	}
 
 	if (exists $parm{BYSETPOS}) {
@@ -382,28 +528,138 @@ sub rrule { # freq, &method(); optional: interval, until, count
 	}
 
 
-	# print " PARAMETERS: ", join(":", %parm), "\n";
+	# everything that was not defined is got from DTSTART
+	unless ($has{month}) {
+		my $tmp = $parm{DTSTART}->month - 1;
+		# print " [ get month from $parm{DTSTART} = $tmp ]\n";
+		$when = $when->offset(mode=>'begin', unit=>'months', value=>[$tmp,$tmp]);
+	}
+	unless ($has{day}) {
+		my $tmp = $parm{DTSTART}->day - 1;
+		$when = $when->offset(mode=>'begin', unit=>'days', value=>[$tmp,$tmp]);
+	}
+	unless ($has{hour}) {
+		my $tmp = $parm{DTSTART}->hour;
+		$when = $when->offset(mode=>'begin', unit=>'hours', value=>[$tmp,$tmp]);
+	}
+	unless ($has{minute}) {
+		my $tmp = $parm{DTSTART}->minute;
+		$when = $when->offset(mode=>'begin', unit=>'minutes', value=>[$tmp,$tmp]);
+	}
+	unless ($has{second}) {
+		my $tmp = $parm{DTSTART}->second;
+		$when = $when->offset(mode=>'begin', unit=>'seconds', value=>[$tmp,$tmp]);
+	}
 
-	# UNTIL and COUNT MUST NOT occur in the same 'recur'
-	if ($parm{UNTIL} ne '') {
-		# UNTIL
-		$when->print(title=>'UNTIL');
-		$rrule = $when->intersection($past, $parm{UNTIL});
-	}
-	elsif ($parm{FREQ} ne '') {
-		# COUNT
-		$when->print(title=>'FREQ');
-		$rrule = $when->intersection(
-			$when->quantize(unit=>$freq{$parm{FREQ}}, strict=>0)
-			->select(freq=>$parm{INTERVAL}, count=>$parm{COUNT}, strict=>0) )
-	}
-	else {
-		$when->print(title=>'no FREQ or UNTIL');
-		$rrule = $when;
-	}
 
-	return $rrule;
+	return $when;
 }
+
+=head2 exrule
+
+    exrule ( period => date-set, DTSTART => time,
+        BYMONTH => [ list ],     BYWEEKNO => [ list ],
+        BYYEARDAY => [ list ],   BYMONTHDAY => [ list ],
+        BYDAY => [ list ],       BYHOUR => [ list ],
+        BYMINUTE => [ list ],    BYSECOND => [ list ],
+        BYSETPOS => [ list ],
+        UNTIL => time, FREQ => freq, INTERVAL => n, COUNT => n,
+		WKST => day )
+
+Implements EXRULE (exclusion-rule) from RFC2445. 
+
+'period' is optional.
+
+=cut
+
+sub exrule { 
+	my $self = shift;
+	unless (ref($self)) {
+		# print " new: $self ";
+		unshift @_, $self;
+		$self = $forever;
+	}
+	my $class = ref($self);
+
+	if (($self->{too_complex}) or ($self->min == -&inf) or ($self->max == &inf)) {
+		my $b = $class->new();
+		$self->trace(title=>"exrule:backtrack"); 
+		# print " [exrule:backtrack] \n" if $DEBUG_BT;
+		$b->{too_complex} = 1;
+		$b->{parent} = $self;
+		$b->{method} = 'exrule';
+		$b->{param}  = \@_;
+		return $b;
+	}
+
+	my %parm = @_;
+	$parm{period} = $self unless $parm{period};
+	my $period = $parm{period};
+	delete $parm{period};
+	# $DEBUG = 1;
+	# print " [ Exclude ", join(':', %parm)," ",$self," ",$parm{period}->rrule(%parm)," ]\n";
+	# print " [   period =     ", $parm{period}," ]\n";
+	# print " [   rrule =      ", $parm{period}->rrule(%parm)," ]\n";
+	# print " [   complement = ", $parm{period}->rrule(%parm)->complement," ]\n";
+	return $self->complement( $period->rrule(%parm) );
+}
+
+
+=head2 rdate
+
+	rdate( list => [time1, time2, ...] )
+
+Adds the (scalar) list to the set, or creates a new list.
+
+=cut
+
+sub rdate { 
+	my $self = shift;
+	unless (ref($self)) {
+		# print " new: $self ";
+		# unshift @_, $self;
+		$self = $never;
+	}
+	my $class = ref($self);
+	my %parm = @_;
+
+	# print " [rdate parm: ",join(':', %parm)," ]\n";
+	# print " [rdate parm: ",join(':', @{$parm{list}} )," ]\n";
+	my @list = ();
+	foreach( @{$parm{list}} ) {
+		push @list, $_, $_;
+	}
+	# print " [rdate list: ",join(':', @list)," = ", $class->new(@list), " ]\n";
+	$self = $self->union( $class->new(@list) );
+	$self->print(title=>'rdate ' . join(':', %parm) );
+	return $self;
+}
+
+
+=head2 exdate
+
+	exdate( list => [time1, time2, ...] )
+
+Removes each element of the list from the set.
+
+=cut
+
+sub exdate { 
+	my $self = shift;
+	my $class = ref($self);
+	my %parm = @_;
+
+	my @list = ();
+	foreach( @{$parm{list}} ) {
+		push @list, $_, $_;
+	}
+
+	$self = $self->complement( $class->new(@list) );
+	$self->print(title=>'exdate ' . join(':', %parm) );
+	return $self;
+}
+
+
 
 =head2 occurrences
 
@@ -446,8 +702,6 @@ Note: 'unit' parameter can be years, months, days, weeks, hours, minutes, or sec
 =head1 BUGS
 
 'rrule' method is not yet fully RFC2445 compliant.
-
-'byday' does not understand (scalar . string) formats yet (like '-2FR')
 
 'duration' and 'period' methods may change in future versions, to generate open-ended sets.
 
--- t/recurr.t
+++ t/recurr.t
@@ -12,6 +12,7 @@ use Test::More qw(no_plan);
 
 BEGIN { use_ok('Date::Set') };
 
+my $a2;
 my $a = Date::Set->new('19971024T120000Z', '19971024T130000Z');
 is("$a", '[19971024T120000Z..19971024T130000Z]', 'simple date set creation works');
 
@@ -28,83 +29,126 @@ $b = $a->dtstart( start => '19971024T113000Z' );
 is("$b",'[19971024T120000Z..19971024T130000Z]',
     "dtstart() can't be set to an earlier date after being set");
 
+$b = $a->dtend( end => '19971024T121000Z' );
+is("$b",'[19971024T120000Z..19971024T121000Z]',
+    "dtend() works to set finish time");
+
 $b = $a->duration( unit => 'days', duration => 5 );
 is("$b",'[19971024T120000Z..19971029T120000Z]',
     "setting the duration of a date set in days works");
 
+$b = Date::Set->rdate( list => [ '19970101Z', '19970102Z' ] );
+is("$b",'19970101Z,19970102Z',
+    "using rdate to create a date list");
+
+$b = Date::Set->rdate( list => [ '19970101Z', '19970102Z' ] )->rdate( list => [ '19970101Z', '19970103Z' ] );
+is("$b",'19970101Z,19970102Z,19970103Z',
+    "using rdate to extend a date list");
+
+$b = Date::Set->rdate( list => [ '19970101Z', '19970102Z' ] )->exdate( list => [ '19970101Z' ] );
+is("$b",'19970102Z',
+    "using exdate to remove a date from a list");
+
 # $a is now a period spanning the year 1997.
-$a = Date::Set->period( time => ['19970101T000000Z', '19971231T235959Z'] );
+$a = Date::Set->period( time => ['19970101Z', '19971231T235959Z'] );
+
+# DTSTART tests
+	# $a2 has 'month'.
+	$a2 = Date::Set->period( time => ['19970506Z', '19981231T235959Z'] );
+	$b = $a2->rrule( FREQ => 'YEARLY' );
+	is("$b",'19970506Z,19980506Z', 
+		"rrule() gets month from DTSTART");
+
+
+# rrule as constructor, without 'period'
+$b = Date::Set->rrule( BYMONTH => [ 10 ] )->occurrences( period => $a );
+is("$b",'19971001Z', 
+	"rrule() constructor is ok");
+
+# rrule as constructor, with 'period'
+$b = Date::Set->rrule( period => $a, BYMONTH => [ 10 ] );
+is("$b",'19971001Z', 
+	"rrule() constructor with period is ok");
+
+
+
+$b = $a->rrule( BYMONTH => [ 9,10,11 ] )->exrule( BYMONTH => [ 10 ] );
+is("$b",'19970901Z,19971101Z', 
+	"exrule() works");
+
+
 
 $b = $a->rrule( BYMONTH => [ 10 ] );
-is("$b",'[19971001Z..19971101Z)', "rrule() interprets BYMONTH correctly");
+is("$b",'19971001Z', 
+	"rrule() interprets BYMONTH correctly");
 
-    $b = $a->rrule( BYMONTH => [ -10 ] );
-    # is( $@, '', "rrule() with a negative BYMONTH runs without dying");
-    # find the 10th month before the end of the year (february)
-    is("$b",'[19970201Z..19970301Z)', 
-            "rrule() interprets BYMONTH with negative numbers correctly");
+$b = $a->rrule( BYMONTH => [ -10 ] );
+# is( $@, '', "rrule() with a negative BYMONTH runs without dying");
+# find the 10th month before the end of the year (february)
+is("$b",'19970201Z', 
+	"rrule() interprets BYMONTH with negative numbers correctly");
     
 # find the 10th week of the year
 $b = $a->rrule( BYWEEKNO => [ 10 ] );
-is("$b",'[19970303Z..19970310Z)',
+is("$b",'19970303Z',
     "rrule() interprets BYWEEKNO with positive numbers correctly");
 
 # figure out the 10th week before the end of the year
 $b = $a->rrule( BYWEEKNO => [ -10 ] );
-is("$b",'[19971013Z..19971020Z)',
+is("$b",'19971013Z',
     "rrule() interprets BYWEEKNO with negative numbers correctly");
 
 # this means "repeat on the 10th day of every year"
 $b = $a->rrule( BYYEARDAY => [ 10 ] );
-is("$b",'[19970110Z..19970111Z)',
+is("$b",'19970110Z',
     "rrule() interprets BYYEARDAY with positive numbers correctly");
 
 # this means "repeat on the day that's 10 days before the end of the year"
 $b = $a->rrule( BYYEARDAY => [ -10 ] );
-is("$b",'[19971221Z..19971222Z)',
+is("$b",'19971221Z',
     "rrule() interprets BYYEARDAY with negative numbers correctly");
 
 # this means "repeat on the 10th day of the month"
 $b = $a->rrule( BYMONTHDAY => [ 10 ] );
-is("$b",'[19970110Z..19970111Z),[19970210Z..19970211Z),[19970310Z..19970311Z),[19970410Z..19970411Z),[19970510Z..19970511Z),[19970610Z..19970611Z),[19970710Z..19970711Z),[19970810Z..19970811Z),[19970910Z..19970911Z),[19971010Z..19971011Z),[19971110Z..19971111Z),[19971210Z..19971211Z)',
+is("$b",'19970110Z,19970210Z,19970310Z,19970410Z,19970510Z,19970610Z,19970710Z,19970810Z,19970910Z,19971010Z,19971110Z,19971210Z',
     'rrule() interprets BYMONTHDAY with positive numbers correctly');
 
 # this means "repeat on the 10th day before the end of every month"
 $b = $a->rrule( BYMONTHDAY => [ -10 ] );
-is("$b",'[19970121Z..19970122Z),[19970218Z..19970219Z),[19970321Z..19970322Z),[19970420Z..19970421Z),[19970521Z..19970522Z),[19970620Z..19970621Z),[19970721Z..19970722Z),[19970821Z..19970822Z),[19970920Z..19970921Z),[19971021Z..19971022Z),[19971120Z..19971121Z),[19971221Z..19971222Z)',
+is("$b",'19970121Z,19970218Z,19970321Z,19970420Z,19970521Z,19970620Z,19970721Z,19970821Z,19970920Z,19971021Z,19971120Z,19971221Z',
     'rrule() interprets BYMONTHDAY with negative numbers correctly');
 
 # all wednesdays
 $b = $a->rrule( UNTIL => '19970201Z', BYDAY => [ qw(WE) ] );
-is("$b",'[19970101T000000Z..19970102Z),[19970108Z..19970109Z),[19970115Z..19970116Z),[19970122Z..19970123Z),[19970129Z..19970130Z)',
+is("$b",'19970101Z,19970108Z,19970115Z,19970122Z,19970129Z',
     'rrule() with BYDAY and UNTIL works correctly');
     
 # last wednesday of month
 $b = $a->rrule( FREQ => 'MONTHLY', UNTIL => '19970301Z', BYDAY => [ qw(-1WE) ] );
-is("$b",'[19970129Z..19970130Z),[19970226Z..19970227Z)', 
+is("$b",'19970129Z,19970226Z', 
     'rrule() with BYDAY and UNTIL works on "last Wednesday of month" syntax (-1WE)');
 
 # last wednesday of year
 $b = $a->rrule( FREQ => 'YEARLY', UNTIL => '19980101Z', BYDAY => [ qw(-1WE) ] );
-is("$b",'[19971231Z..19980101Z)',
+is("$b",'19971231Z',
     'rrule() with BYDAY and UNTIL works on "last Wednesday of year" syntax');
 
 $b = $a->rrule( UNTIL => '19970103Z', BYHOUR => [ 10 ] );
-is("$b",'[19970101T100000Z..19970101T110000Z),[19970102T100000Z..19970102T110000Z)',
+is("$b",'19970101T100000Z,19970102T100000Z',
     'rrule() with UNTIL and BYHOUR ("10-11am every day") works');
     
 # this is not in the RFC
 # $b = $a->rrule( UNTIL => '19970103Z', BYHOUR => [ -10 ] );
 
 $b = $a->rrule( UNTIL => '19970101T030000Z', BYMINUTE => [ 10 ] );
-is("$b",'[19970101T001000Z..19970101T001100Z),[19970101T011000Z..19970101T011100Z),[19970101T021000Z..19970101T021100Z)',
+is("$b",'19970101T001000Z,19970101T011000Z,19970101T021000Z',
     'rrule() with UNTIL and BYMINUTE ("10 minutes after the start of the hour") works');
     
 # this is not in the RFC
 # $b = $a->rrule( UNTIL => '19970101T030000Z', BYMINUTE => [ -10 ] );
 
 $b = $a->rrule( UNTIL => '19970101T000300Z', BYSECOND => [ 10 ] );
-is("$b",'[19970101T000010Z..19970101T000011Z),[19970101T000110Z..19970101T000111Z),[19970101T000210Z..19970101T000211Z)',
+is("$b",'19970101T000010Z,19970101T000110Z,19970101T000210Z',
     'rrule() works with UNTIL and BYSECOND ("the 10th second of every minute")');
     
 # this is not in the RFC
